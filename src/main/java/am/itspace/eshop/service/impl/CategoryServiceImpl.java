3a6d9bec7e2a62db5e02dde426dbe1e70b5195b2
narekgalstyan <narekgalstyan92@gmail.com>
2024-02-21 01:50:52 -0800

    added - I18n Configuration class - messages boundle files - logging
    Modified
    - UserController: added Slf4j
    - CategoryService: added Slf4j
    - register.html: modified language chose option with English, Armenian and Russian; During registration you can see fields with chosen language such as first name, last name
    - application.properties: added logging properties

--- src/main/java/am/itspace/eshop/service/impl/CategoryServiceImpl.java
+++ src/main/java/am/itspace/eshop/service/impl/CategoryServiceImpl.java
@@ -4,12 +4,14 @@ import am.itspace.eshop.entity.Category;
 import am.itspace.eshop.repository.CategoryRepository;
 import am.itspace.eshop.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
@@ -21,6 +23,8 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> findAll() {
-        return categoryRepository.findAll();
+        List<Category> all = categoryRepository.findAll();
+        log.info("find all users, users list size {}", all.size());
+        return all;
     }
 }
